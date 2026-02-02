cc3a278e1fe713611d793210e332f79bac634613
TradeSourceSwitzerland <raul.tito@outlook.com>
2026-02-02 17:37:49 +0100

    Rename health endpoint and change port to 10000

--- api.py
+++ api.py
@@ -10,8 +10,11 @@ def liste():
         return jsonify(json.load(f))
 
 @app.route("/healthz")
-def health():
+def healthz():
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
